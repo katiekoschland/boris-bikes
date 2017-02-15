2ba236a0392e9884be5f3ad947c944a00dff9157
Ashwini <ashie.oh@gmail.com>
2017-02-15 14:57:41 +0000

    First unit test passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,4 +5,7 @@ class DockingStation
     Bike.new
   end
 
+  def dock
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,14 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to :release_bike}
+
   #it { expect(subject.release_bike).to be_an_instance_of Bike}
+
   it 'docking station returning a working bike'do
     bike = subject.release_bike
     expect(bike.working?).to be true
   end
+
+  it {is_expected.to respond_to :dock}
 end
