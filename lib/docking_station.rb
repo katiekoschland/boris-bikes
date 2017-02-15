be02b2c61f3cac2174f0aae93585485c49587c4f
Ashwini <ashie.oh@gmail.com>
2017-02-15 15:49:19 +0000

    Completed 2 feature test and their unit test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,20 @@
 # docking_station = DockingStation
 class DockingStation
+  attr_reader :docked_bikes
+
+  def initialize
+    @docked_bikes = Array.new
+  end
 
   def release_bike
     Bike.new
   end
 
-  def dock
+  def dock(bike)
+  end
+
+  def show_docked
+    docked_bikes
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to :release_bike} # should have a release_bike method
 
   #it { expect(subject.release_bike).to be_an_instance_of Bike}
 
@@ -10,5 +10,15 @@ describe DockingStation do
     expect(bike.working?).to be true
   end
 
-  it {is_expected.to respond_to :dock}
+  it {is_expected.to respond_to :dock} # should have a dock method
+
+  it "check if docking station is receiving a bike when dock is called" do
+    allow(subject).to receive(:dock).with(an_instance_of(Bike)) {raise "station not receiving a Bike object"}
+  end
+
+  it {is_expected.to respond_to :show_docked}
+  it "docking station should return a array of docked bikes when show_docked is called" do
+    expect(subject.show_docked).to be_an_instance_of Array
+  end
+
 end
